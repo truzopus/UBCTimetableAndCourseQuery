a433bdcc83cfefacf530c39777c7bd481dd26019
d6g0b <d6g0b@ugrad.cs.ubc.ca>
2019-10-01 21:53:59 -0700

    refactor addDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -43,85 +43,48 @@ export default class InsightFacade implements IInsightFacade {
         // let datasetMList: string[] = [];
         // let memoDataset = new MemoDataset(datasetMList, datasetMemoList, datasetInMemo);
     }
-
-    // helper function to determine and load valid dataset
-    // private loadValidDataset(id: string, content: string): Promise<boolean> {
-    //     let that = this;
-    //     let con = Buffer.from(content, "base64");
-    //     let zip = new JSZip();
-    //     return zip.loadAsync(con, {base64: true}).then(function (body: any) {
-    //         let p: any[] = [];
-    //         let coursesFolder = body.folder(/courses/);
-    //         if (coursesFolder.length === 1) {
-    //             body.folder("courses").forEach(function (relativePath: any, file: { dir: any; name: string; }) {
-    //                 if (!file.dir) {
-    //                     let jsonFile = body.file(file.name).async("text").then((output: any) => {
-    //                         return Promise.resolve(JSON.parse(output));
-    //                     }).catch(function (error: any) {
-    //                         return Promise.reject(new InsightError("Not JSON file, fail to parse"));
-    //                     });
-    //                     p.push(jsonFile);
-    //                 }
-    //             });
-    //             Promise.all(p).then((result: any) => {
-    //                 let allCourses: any[] = [];
-    //                 for (let courseSec of result) {
-    //                     let courses: any[] = courseSec["result"];
-    //                     for (let course of courses) {
-    //                         if (("Course" in course) && ("Subject" in course) && ("Avg" in course)
-    //                             && ("Title" in course) && ("Professor" in course) && ("Pass" in course)
-    //                             && ("Fail" in course) && ("Year" in course)
-    //                             && ("id" in course) && ("Audit" in course)) {
-    //                             let courseSection: any = {};
-    //                             courseSection["courses_dept"] = course["Subject"];
-    //                             courseSection["courses_id"] = course["Course"];
-    //                             courseSection["courses_avg"] = course["Avg"];
-    //                             courseSection["courses_instructor"] = course["Professor"];
-    //                             courseSection["courses_title"] = course["Title"];
-    //                             courseSection["courses_pass"] = course["Pass"];
-    //                             courseSection["courses_fail"] = course["Fail"];
-    //                             courseSection["courses_audit"] = course["Audit"];
-    //                             courseSection["courses_uuid"] = String(course["id"]);
-    //                             courseSection["courses_year"] = Number(course["Year"]);
-    //                             allCourses.push(courseSection);
-    //                         }
-    //                     }
-    //                 }
-    //                 if (allCourses.length === 0) {
-    //                     return Promise.reject(new InsightError("no valid course section"));
-    //                 } else {
-    //                     let dataset: InsightDataset = {
-    //                         id: id, kind: InsightDatasetKind.Courses,
-    //                         numRows: allCourses.length
-    //                     };
-    //                     that.memoDataset.datasetMemoList.push(dataset);
-    //                     that.memoDataset.datasetInMemo[id] = allCourses;
-    //                     that.memoDataset.datasetMList.push(id)
-    //                     fs.writeFile(diskDir + "/" + id + ".json", JSON.stringify(allCourses), (err: any) => {
-    //                         if (err) {
-    //                             throw err;
-    //                         }
-    //                     });
-    //                     return Promise.resolve(true);
-    //                     // return Promise.resolve(that.memoDataset.datasetMList);
-    //                 }
-    //             }).catch((error: any) => {
-    //                 return Promise.reject(new InsightError("promise.all failed"));
-    //             });
-    //         } else {
-    //             return Promise.reject(new InsightError("invalid dataset subdirectory"));
-    //         }
-    //     }).catch(function (error: any) {
-    //         return Promise.reject(new InsightError("fail to unzip dataset"));
-    //     });
-    // }
-
-    public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+    private sectionCheck (course: any): boolean {
+        let sc: boolean = (("Course" in course) && ("Subject" in course) && ("Avg" in course)
+            && ("Title" in course) && ("Professor" in course) && ("Pass" in course)
+            && ("Fail" in course) && ("Year" in course)
+            && ("id" in course) && ("Audit" in course));
+        return sc;
+    }
+    private datasetKeyConvert(courseSection: any, course: any): void {
+        courseSection["courses_dept"] = course["Subject"];
+        courseSection["courses_id"] = course["Course"];
+        courseSection["courses_avg"] = course["Avg"];
+        courseSection["courses_instructor"] = course["Professor"];
+        courseSection["courses_title"] = course["Title"];
+        courseSection["courses_pass"] = course["Pass"];
+        courseSection["courses_fail"] = course["Fail"];
+        courseSection["courses_audit"] = course["Audit"];
+        courseSection["courses_uuid"] = String(course["id"]);
+        courseSection["courses_year"] = Number(course["Year"]);
+    }
+    private updateMemory(id: string, allCourses: any, memoDataset: MemoDataset): void {
+        let dataset: InsightDataset = {
+            id: id, kind: InsightDatasetKind.Courses,
+            numRows: allCourses.length
+        };
+        memoDataset.datasetMemoList.push(dataset);
+        memoDataset.datasetInMemo[id] = allCourses;
+        memoDataset.datasetMList.push(id);
+    }
+    private invalidInputCheck(id: string, content: string, kind: InsightDatasetKind): boolean {
         if (/^\s+$/.test(id) || id === null || id === undefined ||
             kind === null || kind === undefined ||
-            /^\s+$/.test(content) || content === null || content === undefined) {
-            return Promise.reject(new InsightError("empty or null or undefined input parameter"));
-        } else if (id.includes(("_")) || (kind !== InsightDatasetKind.Courses && kind !== InsightDatasetKind.Rooms)) {
+            /^\s+$/.test(content) || content === null || content === undefined){
+            return true;
+        } else if (id.includes(("_")) || (kind !== InsightDatasetKind.Courses && kind !== InsightDatasetKind.Rooms)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+        if (this.invalidInputCheck(id, content, kind)) {
             return Promise.reject(new InsightError("invalid input parameter"));
         } else if (this.memoDataset.datasetMList.includes(id)) {
             return Promise.reject(new InsightError("dataset already added"));
@@ -140,46 +103,40 @@ export default class InsightFacade implements IInsightFacade {
                             }).catch(function (error: any) {
                                 return Promise.reject(new InsightError("Not JSON file, fail to parse"));
                             });
-                            p.push(jsonFile); }});
+                            p.push(jsonFile);
+                        }
+                    });
                     Promise.all(p).then((result: any) => {
                         let allCourses: any[] = [];
                         for (let courseSec of result) {
-                            let courses: any[] = courseSec["result"];
-                            for (let course of courses) {
-                                if (("Course" in course) && ("Subject" in course) && ("Avg" in course)
-                                    && ("Title" in course) && ("Professor" in course) && ("Pass" in course)
-                                    && ("Fail" in course) && ("Year" in course)
-                                    && ("id" in course) && ("Audit" in course)) {
+                            for (let course of courseSec["result"]) {
+                                if (that.sectionCheck(course)) {
                                     let courseSection: any = {};
-                                    courseSection["courses_dept"] = course["Subject"];
-                                    courseSection["courses_id"] = course["Course"];
-                                    courseSection["courses_avg"] = course["Avg"];
-                                    courseSection["courses_instructor"] = course["Professor"];
-                                    courseSection["courses_title"] = course["Title"];
-                                    courseSection["courses_pass"] = course["Pass"];
-                                    courseSection["courses_fail"] = course["Fail"];
-                                    courseSection["courses_audit"] = course["Audit"];
-                                    courseSection["courses_uuid"] = String(course["id"]);
-                                    courseSection["courses_year"] = Number(course["Year"]);
-                                    allCourses.push(courseSection); }}}
-                        if (allCourses.length === 0) {
-                            return Promise.reject(new InsightError("no valid course section"));
-                        } else {
-                            let dataset: InsightDataset = {
-                                id: id, kind: InsightDatasetKind.Courses,
-                                numRows: allCourses.length};
-                            that.memoDataset.datasetMemoList.push(dataset);
-                            that.memoDataset.datasetInMemo[id] = allCourses;
-                            that.memoDataset.datasetMList.push(id);
+                                    that.datasetKeyConvert(courseSection, course);
+                                    allCourses.push(courseSection);
+                                }
+                            }
+                        }
+                        if (allCourses.length >= 0) {
+                            that.updateMemory(id, allCourses, that.memoDataset);
                             fs.writeFile(diskDir + "/" + id + ".json", JSON.stringify(allCourses), (err: any) => {
-                                if (err) {throw err; }});
-                            return Promise.resolve(that.memoDataset.datasetMList); }
+                                if (err) {
+                                    throw err;
+                                }
+                            });
+                            return Promise.resolve(that.memoDataset.datasetMList);
+                        }
                     }).catch((error: any) => {
-                        return Promise.reject(new InsightError("promise.all failed")); });
-                } else { return Promise.reject(new InsightError("invalid dataset subdirectory")); }
+                        return Promise.reject(new InsightError("promise.all failed"));
+                    });
+                } else {
+                    return Promise.reject(new InsightError("invalid dataset subdirectory"));
+                }
             }).catch(function (error: any) {
-                return Promise.reject(new InsightError("fail to unzip dataset")); });
-        }}
+                return Promise.reject(new InsightError("fail to unzip dataset"));
+            });
+        }
+    }
 
     public removeDataset(id: string): Promise<string> {
         return Promise.reject("Not implemented.");
@@ -191,7 +148,6 @@ export default class InsightFacade implements IInsightFacade {
 
     public listDatasets(): Promise<InsightDataset[]> {
         return Promise.resolve(this.memoDataset.datasetMemoList);
-        // return Promise.reject("Not implemented.");
 
     }
 }
